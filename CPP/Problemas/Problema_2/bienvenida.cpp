fe63a022c3d515b3e1a81ac46fe971af5edb2db7
ALAND879 <adjr879@gmail.com>
2023-02-21 18:01:00 -0600

    deleted:    CPP/Problemas/Problema_2/Filtrando_multiplos.cpp new file:   CPP/Problemas/Problema_2/bienvenida.cpp

---
+++ CPP/Problemas/Problema_2/bienvenida.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string nombre ;
+    getline(cin, nombre);
+    cout << "Bienvenido al sistema " << nombre << ".";
+}
